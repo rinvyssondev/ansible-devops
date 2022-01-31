67c7c873ca435eeb225a4faf121f102ce60cfa11
Rinvysson.dev <rinvysson.dev@hotmail.com>
2022-01-31 17:54:31 -0300

    Treinando ansible

---
+++ app01/provision.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cat <<EOT >> /home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQClfGb0slvT/oHeOK+DDV8lBtBYou1m/rdo6AGLqYlXEPPC8/UGrasgvvtujQltP++8luV7C/T/ENvx2ZydYkvJr8FmssQ/WsjquV+vZxAh85/PsdoR/xNHzo3sw+0D0Y24tzH73264pxHO26NBjCrqpWWkYK0KrlFeYM5a6fOoGVlAG/iGfGYhEImI4VspAcezEHsPKNbETYlK5/kcUPqF3smw01ujzWxhylrmGOaUduEWN3lY0jDVNDH+nTD1Yc4uYYkczmoArvGBtplwe4Je2T9BMRQ2ru/yoyG4JY+sqzMux/zOWMOS2sb9YeG7uXAt/ncQbk/YHTa3NqcevSA1 vagrant@control-node
+EOT
